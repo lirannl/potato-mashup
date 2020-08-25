3182547f7f0061b0fa72cccf9fbcdca04dd22d8e
Liran Piade <liranpiade@gmail.com>
2020-08-26 01:46:05 +1000

    Removed unused files

--- src/interfaces/patentData.ts
+++
@@ -1,9 +0,0 @@
-import { IPerson } from "./person";
-import ISentiment from "./sentiment";
-import { safePatent } from "./usPatentRespose";
-
-export interface DocData {
-    patent:    safePatent;
-    person:    IPerson;
-    sentiment: ISentiment;
-}
